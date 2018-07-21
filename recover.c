e3aa75bd2a373b74c74d84abd93f99cb847737a2
Wolfsta <wolfsta@users.noreply.github.com>
2018-07-21 13:44:01 -0700

    Update recover.c

--- recover.c
+++ recover.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 typedef uint8_t BYTE;
 
@@ -23,22 +24,13 @@ int main(int argc, char *argv[])
 
     //initialize needed variables and file for writing
     int jpgcount = 0;
-    char *outfile = NULL;
+    char *outfile = malloc((sizeof(char)) * 7);
     BYTE buffer[512];
     FILE *img = NULL;
 
-    do  {
-        // read file to buffer one byte at a time so can see when it reads less than 512bytws at the end
-        fread(buffer, 1, 512, infile);
+    // read file to buffer one byte at a time so can see when it reads less than 512bytes at the end
+    while((fread(buffer, 1, 512, infile)) == 512)  {
 
-        /* may need  to do it this way
-        if (fread(buffer, 1, 512, infile) != 512)
-        {
-            fclose(img);
-            fclose(infile);
-            break;
-        }
-        */
 
         // check if its a jpg file
         if (buffer[0] == 0xff &&
@@ -49,44 +41,54 @@ int main(int argc, char *argv[])
             // it is a jpg so check if no jpg exists yet, write buffer and increment jpgcount
             if(jpgcount == 0)
             {
-                sprintf (outfile, "%03i.jpg", jpgcount);
-                img = fopen(outfile, "w");
-                fwrite(buffer, 1, 512, img);
-
-            // else if it is a new jpg but not the first one, close the open file, create a new filename, open it for writing, write the buffer and increment jpgcount
-            } else {
-                fclose(img);
-                sprintf (outfile, "%03i.jpg", jpgcount);
+                jpgcount++;
+                sprintf (outfile, "%03i.jpg", (jpgcount - 1));
                 img = fopen(outfile, "w");
                 if (img == NULL)
                 {
                     fprintf(stderr, "Cannot open file\n");
-                    return 2;
+                    return 3;
                 }
                 fwrite(buffer, 1, 512, img);
+
+
+            // else if it is a new jpg but not the first one, close the open file, create a new filename, open it for writing, write the buffer and increment jpgcount
+            } else {
                 jpgcount++;
+                fclose(img);
+                sprintf (outfile, "%03i.jpg", (jpgcount - 1));
+                img = fopen(outfile, "w");
+                if (img == NULL)
+                {
+                    fprintf(stderr, "Cannot open file\n");
+                    return 4;
+                }
+                fwrite(buffer, 1, 512, img);
             }
 
 
         // its not a new jpg so else if its not the first jpg found then close the writing file, reopen it in append mode and and write the buffer
-        } else if (jpgcount != 0)
+        } else if (jpgcount >= 1)
         {
             fclose(img);
             img = fopen(outfile, "a");
             if (img == NULL)
                 {
                     fprintf(stderr, "Cannot open file\n");
-                    return 2;
+                    return 5;
                 }
+
             fwrite(buffer, 1, 512, img);
 
+
         // else would be to do nothing as its not a new jpg and its not for an existing file, so can I leave this blank and it will go back to top of loop?
+        } else {
+
         }
 
-    //while not end of file, if it is then close the infile and img files
-    } while (!(feof(img)));
-
-    fclose(infile);
+    }
     fclose(img);
+    fclose(infile);
+    free(outfile);
 
 }
